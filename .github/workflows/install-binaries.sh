091fbc2aa278230802245b8e1562994ff09e750d
Remco van 't Veer <remco@remworks.net>
2025-05-13 13:36:17 +0200

    Upgrade deps

--- .github/workflows/install-binaries.sh
+++ .github/workflows/install-binaries.sh
@@ -2,6 +2,7 @@
 
 # SPDX-FileCopyrightText: 2024 SURF B.V.
 # SPDX-FileContributor: Joost Diepenmaat
+# SPDX-FileContributor: Remco van 't Veer
 #
 # SPDX-License-Identifier: Apache-2.0
 
@@ -9,10 +10,7 @@
 
 set -ex
 
-CLOJURE_VERSION="1.11.1.1273"
-
 if [ ! -x "bin/clojure" ]; then
-    curl -O "https://download.clojure.org/install/linux-install-${CLOJURE_VERSION}.sh"
-    bash "linux-install-${CLOJURE_VERSION}.sh" -p "$(pwd)"
+    curl -L -O https://github.com/clojure/brew-install/releases/latest/download/posix-install.sh
+    bash posix-install.sh -p "$(pwd)"
 fi
-
